06a3d5e35367f69e64de1c2a7811c8fa7f7a4d93
Tristan Kennedy <tdouglaskennedy@gmail.com>
2022-10-31 11:10:17 -0500

    Removed Jumping and Crouching

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,8 +10,6 @@ public class PlayerMovement : MonoBehaviour
     public float runSpeed = 40f;
 
     float horizontalMove = 0f;
-    bool jump = false;
-    bool crouch = false;
 
     // Update is called once per frame
     void Update()
@@ -19,26 +17,11 @@ public class PlayerMovement : MonoBehaviour
 
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
-        if (Input.GetButtonDown("Jump"))
-        {
-            jump = true;
-        }
-
-        if (Input.GetButtonDown("Crouch"))
-        {
-            crouch = true;
-        }
-        else if (Input.GetButtonUp("Crouch"))
-        {
-            crouch = false;
-        }
-
     }
 
     void FixedUpdate()
     {
         // Move our character
-        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
-        jump = false;
+        controller.Move(horizontalMove * Time.fixedDeltaTime);
     }
 }
